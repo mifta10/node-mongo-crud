0c0123b6dd2fe6e8a894c9907dfbd6f479d85517
mifta10 <miftauddinkhan10@gmail.com>
2021-06-03 23:44:04 +0600

    mongodb crud basic

---
+++ index.js
@@ -0,0 +1,78 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const MongoClient = require('mongodb').MongoClient;
+const ObjectId = require('mongodb').ObjectId;
+
+const password = 'mifta';
+
+// Connection URL
+const uri = "mongodb+srv://mifta10:mifta@cluster0.8pac6.mongodb.net/organicdb?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+//express app
+const app = express();
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }));
+
+//frontend connect
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+})
+
+
+// Use connect method to connect to the Server
+client.connect(err => {
+  const productCollection = client.db("organicdb").collection("products");
+
+
+  //Create Product To Database
+  app.post('/addProduct', (req, res) => {
+    const product = req.body;
+    //console.log(product)
+    productCollection.insertOne(product)
+    .then(result => {
+      console.log("Added Successfully");
+      res.redirect('/')
+    })
+  })
+
+  //Read Products from Database
+  app.get('/products', (req, res) => {
+    productCollection.find({}).toArray((err, documents) => {
+      res.send(documents);
+    })
+  })
+
+  //single product load
+  app.get('/product/:id', (req, res) => {
+    productCollection.find({ _id: ObjectId(req.params.id) })
+      .toArray((err, documents) => {
+        res.send(documents[0]);
+      })
+  })
+
+  app.patch('/update/:id', (req, res) => {
+    productCollection.updateOne({_id: ObjectId(req.params.id)},
+    {
+      $set: {price: req.body.price, quantity: req.body.quantity}
+    })
+    .then (result => {
+      res.send(result.modifiedCount > 0)
+    })
+  })
+
+
+  //Delete Product from Database
+  app.delete('/delete/:id', (req, res) => {
+    productCollection.deleteOne({ _id: ObjectId(req.params.id) })
+      .then(result => {
+        console.log(result);
+        res.send(result.deletedCount > 0);
+      })
+  })
+
+  console.log('database connected');
+});
+
+
+app.listen(3000);
