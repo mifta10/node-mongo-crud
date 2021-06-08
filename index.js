1786f8d1607aaaa24101da889c47c8c3fb4c2a72
mifta10 <miftauddinkhan10@gmail.com>
2021-06-08 09:02:35 +0600

    final

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const MongoClient = require('mongodb').MongoClient;
 const ObjectId = require('mongodb').ObjectId;
+require('dotenv').config()
 
-const password = 'mifta';
 
 // Connection URL
-const uri = "mongodb+srv://mifta10:mifta@cluster0.8pac6.mongodb.net/organicdb?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.8pac6.mongodb.net/organicdb?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 //express app
